8279401a3191b74e88a0b7a3a46e890a9283e695
Trần Nam <sam@google.com>
2020-11-27 14:35:01 +0700

    first commit

---
+++ app/src/main/java/com/example/maytinh/ui/home/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.example.maytinh.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.maytinh.R;
+
+public class HomeFragment extends Fragment {
+
+
+
+}
